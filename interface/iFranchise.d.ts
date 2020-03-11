d6e55e1431eb1da8fa0258c78b56412dc4248320
Nathan Beesley <natebeesley@live.com>
2020-03-10 20:43:21 -0500

    u

--- interface/iFranchise.d.ts
+++ interface/iFranchise.d.ts
@@ -1,5 +1,8 @@
 export interface franchise {
     franchiseId?: number;
+    firstSeasonId: number;
+    mostRecentTeamId: number;
     teamName?: string;
+    locationName?: string;
     link?: string;
 }
